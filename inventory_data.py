99bee24c6e4c22f2e0449191e31c139b7b9b3ee2
Jamie Bryson <jamieab98@gmail.com>
2026-02-12 13:17:34 -0500

    created inventory_data.py and added one product as a template.

---
+++ inventory_data.py
@@ -0,0 +1,10 @@
+inventory = [
+    {
+        "product name": "Skittles Crazy Sour",
+        "quantity": 1,
+        "barcode_number": "5000159376792",
+        "category": "snacks",
+        "brand": "Skittles",
+        "id": 1
+    }
+]
