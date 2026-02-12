6c4662dde5759f390c4564a96e013e1f648cbb55
Jamie Bryson <jamieab98@gmail.com>
2026-02-12 14:04:17 -0500

    Added another item to the inventory and ade the /inventory route. Made the route show the product name and quantity for each item

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 from flask import Flask
+from inventory_data import inventory
 
 app = Flask(__name__)
+
+@app.route("/")
+def default():
+    return "Welcome to the inventory!"
+
+@app.route("/inventory")
+def view_inventory():
+    for item in inventory:
+        print(f"Product: {item['product_name']}")
+        print(f"Quantity: {item['quantity']}")
+
+view_inventory()
--- inventory_data.py
+++ inventory_data.py
@@ -1,10 +1,18 @@
 inventory = [
     {
-        "product name": "Skittles Crazy Sour",
+        "product_name": "Skittles Crazy Sour",
         "quantity": 1,
         "barcode_number": "5000159376792",
-        "category": "snacks",
+        "category": "Snacks",
         "brand": "Skittles",
         "id": 1
+    },
+    {
+        "product_name": "Frosted Cinnamon Pop-Tart",
+        "quantity": 4,
+        "barcode_number": "0038000551246",
+        "category": "Snacks",
+        "brand": "Pop-Tart",
+        "id": 2
     }
 ]
