d08363155c65cab7544a96a742c1e5af4a30ae61
Jamie Bryson <jamieab98@gmail.com>
2026-02-12 16:28:48 -0500

    created the rout to show a specific item in the inventory

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from inventory_data import inventory
 
 app = Flask(__name__)
@@ -9,8 +9,17 @@ def default():
 
 @app.route("/inventory")
 def view_inventory():
+    response = []
     for item in inventory:
+        response.append({"product": item['product_name'], "quantity": item['quantity']})
         print(f"Product: {item['product_name']}")
         print(f"Quantity: {item['quantity']}")
+    return jsonify(response), 200
 
-view_inventory()
+@app.route("/inventory/<int:id>")
+def view_item(id):
+    item = next((i for i in inventory if i['id'] == id), None)
+    print(item)
+    return jsonify(item), 200
+
+view_item(2)
